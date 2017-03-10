30b310cdaa326687810a8d2686b46e2f780a364a
Jakub <2133142k@student.gla.ac.uk>
2017-03-10 15:48:31 +0000

    5

--- museapp/views.py
+++ museapp/views.py
@@ -1,17 +1,88 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from mus.models import MusicProject, Comment, UserProfile
+from mus.forms import CoomentForm, MusicProjectForm, UserForm, UserProfileForm
 
-# Create your views here.
-def testBase(request):
-    context_dict = {"SignedIn":False, "Username": "Alice"}
-    return render( request, "muse/base.html", context_dict)
+def about(request):
+    return HttpResponse("This webapp is ment for musicians to share their work and to get from other user on how to improve.")
 
-def homePage(request):
-    context_dict = {"SignedIn":False, "Username": "Alice",
-                    "IntrductionText":"Welcome to Muse a website to share your musical ideas",
-                    "ProjectPreviews":[{"ProjectName":"13 bar blues",
-                                        "ProjectAuthor":"UnluckyCoolCat24",
-                                        "ProjectGenre":"Blues",
-                                        "NumberOfComments":2,
-                                        "ProjectDescription":"Like the 12 bar blues but with an added bar!"},
-                                       ],}
-    return render ( request, "muse/home.html", context_dict)
+def home(request):
+    project_list = MusicProject.objects()
+    context_dict = {'projects': project_list}
+    return render(request,'home.html',context=context_dict)
+
+def myAccount(request):
+    project_list = MusicProject.objects.get(user_iexact=request.user.username)
+    context_dict = {'projects':project_list}
+    return render(request,'login/myaccount.html',context=context_dict)
+
+def musicProject(request,musicproject_name_slug,user_name_slug):
+    context_dict = {}
+    try:
+        musicProject = MusicProject.objects.get(slug=musicproject_name_slug)
+        user = User.objects.get(slug=user_name_slug)
+        comments = Comment.objects.filter(project=musicProject)
+        context_dict['comments'] = comments
+        context_dict['musicProject'] = musicProject
+        context_dict['user'] = user
+    except MusicProject.DoesNotExist:
+        context_dict['comments'] = none
+        context_dict['musicProject'] = none
+        context_dict['user']=none
+    return render(request, 'projects/user/musicProject.html', context_dict)
+
+def createProject(request):
+    context = RequestContext(request.POST)
+
+    if request.method=='POST':
+        form = MusicProjectForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            return myAccount(request)
+        else:
+            print form.errors
+    else:
+        form = MusicProjectForm()
+    return render_to_response('projects/newproject.html')
+
+#def login(request):
+ #   username = request.POST['username']
+  #  password = request.POST['password']
+   # user = authenticate(username=username,password=password)
+    #if user is not None:
+     #   login(request,user)
+
+#    else:
+ #       print("Invalid username or password")
+  #  return
+
+def register(request):
+    registered = False
+    if request.method =='POST':
+        user_form = UserForm(data=request.POST)
+        profile_form = UserProfilerForm(data=request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+
+            user.set_password(user.password)
+            user.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user=user
+
+            if 'picture' in request.FILES:
+                profile.picture = request.FILES['picture']
+
+            profile.save
+
+            registered=true
+        else:
+            print(user_form.errors, profile_form.errors)
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+    return render(request,'/signup.html',{'user_form':user_form,'profile_form':profile_form,'registered':registered})
+
+
+
