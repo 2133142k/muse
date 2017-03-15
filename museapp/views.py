fb3d8fa556026297494f5e8ac4e12d2afdeb6dea
Angus McIntosh <angusmcintosh5@gmail.com>
2017-03-15 21:21:12 +0000

    added extra views and linked urls to them

--- museapp/views.py
+++ museapp/views.py
@@ -31,7 +31,7 @@ def testAccountPage(request):
 
 def testProjectViewPage(request):
     #should only be accessible while logged in
-    context_dict = {"SingedIn":True, "Username":"Alice",
+    context_dict = {"SignedIn":True, "Username":"Alice",
                     "Project":{"ProjectName":"13 bar blues",
                                "ProjectAuthor":"UnluckyCoolCat24",
                                "ProjectGenre":"Blues",
@@ -54,3 +54,87 @@ def about(request):
     context_dict = {"SignedIn":False}
 
     return render ( request, "muse/about.html", context_dict)
+	
+def getProjectPreviews(request):
+    response = getUserInfo(request)
+    number = request.GET.get('number')
+    if (not number.isdigit()):
+        number = 5
+    else:
+        number = int(number)
+    if (request.GET.get('owned') == 'true'):
+        #get only owner's projects
+        #reduce to number responses
+        pass
+    else:
+        #return number random projects
+        pass
+
+
+    #return projects JSON
+    response["ProjectPreviews"] = [{"ProjectName":"13 bar blues",
+                               "ProjectAuthor":"UnluckyCoolCat24",
+                               "ProjectGenre":"Blues",
+                               "NumberOfComments":2,
+                               "ProjectDescription":"Like the 12 bar blues but with an added bar!"}]
+    return JsonResponse(response)
+
+def project(request, project_name_slug):
+    context_dict = getUserInfo(request)
+    if (request.method == "GET"):
+        #get a project
+        context_dict["Project"] = {"ProjectName":"13 bar blues",
+                               "ProjectAuthor":"UnluckyCoolCat24",
+                               "ProjectGenre":"Blues",
+                               "NumberOfComments":2,
+                               "ProjectDescription":"Like the 12 bar blues but with an added bar!",
+                               "AudioFile":"13-bar-blues.mp3",
+                               "ExtraFiles":[{"filename":"13-bar-blues-sheetmusic.pdf","filetype":"Sheetmusic"}]}
+
+        return render ( request, "muse/projectView.html", context_dict)
+
+    elif (request.method == "POST"):
+        #not yet implemented
+        #new project
+        return HttpResponseNotModified()
+
+    elif (request.method == "DELETE"):
+        #not yet implemented
+        #delete project
+        return HttpResponseNotModified()
+
+    elif (request.method == "PUT"):
+        #not yet implemented
+        #edit project
+        return HttpResponseNotModified()
+
+    else:
+        #not a valid request
+        return HttpResponseBadRequest()
+
+def userPage(request):
+    context_dict = getUserInfo(request)
+    if (request.method == "GET"):
+        #get an accountPage
+        #check username_slug == page's username
+        return render(request, "muse/accountPage.html", context_dict)
+    
+    elif (request.method == "POST"):
+        #not yet implemented
+        #new user
+        return HttpResponseBadRequest()
+
+    elif (request.method == "DELETE"):
+        #not yet implemented
+        #delete account
+        return HttpResponseBadRequest()
+
+    elif (request.method == "PUT"):
+        #not yet implemented
+        #change password
+        return HttpResponseBadRequest()
+
+def getUserInfo(request):
+    return {"SignedIn":True, "Username":"Alice"}
+    
+        
--- static/javascript/projectPreviews.js
+++ static/javascript/projectPreviews.js
@@ -2,59 +2,60 @@
 
 
 
-function insertProjectPreviews(projectPreviews, isLoggedIn, canEdit){
+function insertProjectPreviews(inputDict){
 
-	var numberOfPreviews = projectPreviews.projectPreviews.length;
-	var newPreviewsHTML = "";
+	var isLoggedIn = inputDict.LoggedIn;
+	var projectPreviews = inputDict.ProjectPreviews;
+	var numberOfPreviews = projectPreviews.length;
 	var projectPreviewsBlock = document.getElementById("projectPreviews");
-	var previewDiv;
-	var tempElement;
-	var tempText;
 	var i;
 
 	for (i = 0; i < numberOfPreviews; i = i + 1){
 		
-		projectPreview = projectPreviews.projectPreviews[i];
-		
-		previewDiv = document.createElement("div");
-		previewDiv.className ="projectPreview";
-		
-		
-		
-		tempElement = document.createElement("h3");
-		tempText = document.createTextNode(projectPreview.ProjectName);
-		tempElement.appendChild(tempText);
-		previewDiv.appendChild(tempElement);
-		
-		tempElement = document.createElement("p");
-		tempText = document.createTextNode(projectPreview.ProjectAuthor);
-		tempElement.appendChild(tempText);
-		previewDiv.appendChild(tempElement);
-		
-		tempElement = document.createElement("p");
-		tempText = document.createTextNode(projectPreview.ProjectGenre);
-		tempElement.appendChild(tempText);
-		previewDiv.appendChild(tempElement);
-		
-		tempElement = document.createElement("p");
-		tempText = document.createTextNode("Comments - " + projectPreview.NumberOfComments);
-		tempElement.appendChild(tempText);
-		previewDiv.appendChild(tempElement);
-		
-		tempElement = document.createElement("p");
-		tempText = document.createTextNode(projectPreview.ProjectDescription);
-		tempElement.appendChild(tempText);
-		previewDiv.appendChild(tempElement);
-		
-		addProjectPreviewAccessButtons(previewDiv, isLoggedIn, canEdit);
-		projectPreviewsBlock.appendChild(previewDiv);
-		
-		
+		projectPreview = projectPreviews[i];
+		addProjectPreview(projectPreview, projectPreviewsBlock, isLoggedIn);
 	}
 
 }
 
-function addProjectPreviewAccessButtons(previewDiv, isLoggedIn, canEdit){
+function addProjectPreview(projectPreview, parentDiv, isLoggedIn){
+	
+	var tempElement;
+	var tempText;
+	var previewDiv = document.createElement("div");
+	previewDiv.className ="projectPreview";
+	previewDiv.id ="projectReview"+projectPreview.nameSlug;
+	
+	tempElement = document.createElement("h3");
+	tempText = document.createTextNode(projectPreview.ProjectName);
+	tempElement.appendChild(tempText);
+	previewDiv.appendChild(tempElement);
+	
+	tempElement = document.createElement("p");
+	tempText = document.createTextNode(projectPreview.ProjectAuthor);
+	tempElement.appendChild(tempText);
+	previewDiv.appendChild(tempElement);
+	
+	tempElement = document.createElement("p");
+	tempText = document.createTextNode(projectPreview.ProjectGenre);
+	tempElement.appendChild(tempText);
+	previewDiv.appendChild(tempElement);
+	
+	tempElement = document.createElement("p");
+	tempText = document.createTextNode("Comments - " + projectPreview.NumberOfComments);
+	tempElement.appendChild(tempText);
+	previewDiv.appendChild(tempElement);
+	
+	tempElement = document.createElement("p");
+	tempText = document.createTextNode(projectPreview.ProjectDescription);
+	tempElement.appendChild(tempText);
+	previewDiv.appendChild(tempElement);
+	
+	addProjectPreviewAccessButtons(previewDiv, isLoggedIn, projectPreview.canEdit, projectPreview.nameSlug);
+	parentDiv.appendChild(previewDiv);
+}
+
+function addProjectPreviewAccessButtons(previewDiv, isLoggedIn, canEdit, slug){
 	var tempElement;
 	var tempText;
 	var divBlock = document.createElement("div");
@@ -66,22 +67,24 @@ function addProjectPreviewAccessButtons(previewDiv, isLoggedIn, canEdit){
 		tempElement = document.createElement("button");
 		tempText = document.createTextNode("View");
 		tempElement.appendChild(tempText);
-		//add view event!!! Should either be link to view page or load in the project and display
+		tempElement.setAttribute("onclick", "javascript: location.assign('/muse/projects/"+ slug + "'");//link to projectPage
 		divBlock.appendChild(tempElement);
 		
 		if (canEdit){
 			
 			//Edit button
-			tempElement = document.createElement("button");
-			tempText = document.createTextNode("Edit");
-			tempElement.appendChild(tempText);
+			//tempElement = document.createElement("button");
+			//tempText = document.createTextNode("Edit");
+			//tempElement.appendChild(tempText);
 			//add edit event!!! Should either be link to edit page or load in the project and display with edit controls
-			divBlock.appendChild(tempElement);
+			//divBlock.appendChild(tempElement);
 			
 			//Delete button
 			tempElement = document.createElement("button");
 			tempText = document.createTextNode("Delete");
 			tempElement.appendChild(tempText);
+			tempElement.setAttribute("onclick", "javascript: deleteProject(" + projectSlug + ")");
+			
 			//add delete event!!! Should ask if user wants to delete then request server to delete
 			divBlock.appendChild(tempElement);
 		}
@@ -97,25 +100,42 @@ function addProjectPreviewAccessButtons(previewDiv, isLoggedIn, canEdit){
 	}
 }
 
+function deleteProject(projectSlug){
+	//confirm delete
+	var confirmed = window.confirm("Are you sure you want to delete this project?");
+	if (!confirmed){return;}
+	
+	//request delete
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function(){
+		if (this.readyState == 4 && this.status == 200){
+			var deleted = document.getElementById("projectPreview" + projectSlug);
+			deleted.parentNode.removeChild(deleted);
+		}
+	}
+	xhttp.open("DELETE","/muse/projects/"+projectSlug+"/");
+	xhttp.send();
+}
+
 //functionChangeLoggedIn
 
-function getStartingProjectPreviews(){
+function getStartingProjectPreviews(number){
 	//change to request from server
-	var projectPreviews = '{"projectPreviews":[{"ProjectName":"13 bar blues",' +
-                                        '"ProjectAuthor":"UnluckyCoolCat24",' +
-                                        '"ProjectGenre":"Blues",' +
-                                        '"NumberOfComments":"2",' +
-	'"ProjectDescription":"Like the 12 bar blues but with an added bar!"}]}';
-	projectPreviews = JSON.parse(projectPreviews);
-
-	return projectPreviews;
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function(){
+		if (this.readyState == 4 && this.status == 200){
+			//alert(this.responseText);
+			insertProjectPreviews(JSON.parse(this.responseText));
+		}
+	}
+	xhttp.open("GET","/muse/projects/?owned=true&number=" + number,true);
+	xhttp.send();
 }
 
 function onPageLoad(isLoggedIn, username, projectAuthor){
 	
-	var loggedIn = false;//get logged in data
-	var canEdit = false;//(username == projectAuthor);can edit if user owns the page
-	insertProjectPreviews(getStartingProjectPreviews(), loggedIn, canEdit);
+	var number = 5;
+	getStartingProjectPreviews(number);
 	}
 	
 window.onload = onPageLoad(); 
